c3c181930df146126984be8ebf91f27215a6a568
DinDjarin247 <space1429@gmail.com>
2024-05-23 11:05:44 +0900

    do

--- react0003/src/Area.jsx
+++
@@ -1,9 +0,0 @@
-const Area = (props) => {
-  return (
-    <h2 id="location">
-      {props.data.name}/{props.data.lat}/{props.data.lon}
-    </h2>
-  );
-};
-
-export default Area;
--- react0003/src/secret.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "8482ea76f9116553283ef94f1ce9be09";
-
-export {API_KEY};
